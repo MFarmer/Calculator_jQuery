04a7420145fc7b89dcd9daf843f4165f68d794c2
Matthew Farmer <matthew.farmer@icloud.com>
2014-07-12 19:49:38 -0400

    Add more operation functionality and improved UI colors

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,4 +1,14 @@
 var calculator = {
+
+  values: [],
+
+  engagedButtons: [],
+
+  clearOnNextInput: false,
+
+  lastOperation: null,
+
+
   init: function() {
     calculator.setup();
   },
@@ -7,7 +17,6 @@ var calculator = {
     // Add click listeners to digit buttons
     for(var i=0; i<10; i++){
       $("[data-id='"+i+"']").click(function(event){
-        //console.log($(event.currentTarget).attr("data-id"));
         calculator.updateOutput($(event.currentTarget).attr("data-id"));
       });
     }
@@ -16,32 +25,107 @@ var calculator = {
       calculator.resetOutput();
     });
 
-    $("[data-id='+']").click(function(event){
-      $("[data-id='+']").css("background-color","black");
+    $("[data-id='=']").click(function(event){
+      if(calculator.values.length > 0){
+        calculator.values.push($("#output").html());
+        calculator.engagedButtons = [];
+        calculator.performCalculation(calculator.lastOperation);
+      }
+    });
 
-      calculator.operation = "+";
+    $("[data-id='+']").click(function(event){
+      calculator.handleOperation("+");
+    });
+
+    $("[data-id='-']").click(function(event){
+      calculator.handleOperation("-");
+    });
+
+    $("[data-id='/']").click(function(event){
+      calculator.handleOperation("/");
+    });
+
+    $("[data-id='*']").click(function(event){
+      calculator.handleOperation("*");
+    });
+
+    $("[data-id='+/-']").click(function(event){
+      var output = $("#output").html();
+
+      if(output[0] === "-"){
+        output = output.substr(1,output.length-1);
+      } else {
+        output = "-" + output;
+      }
+
+      $("#output").html(output);
     });
   },
 
-  updateOutput: function(input) {
+  performCalculation: function(op) {
+    var result = 0;
 
-    //Is an operation queued?
-    if(calculator.operation && calculator.operation.length > 0){
-      console.log("op in queue!");
+    if(op === "+"){
+      result = parseInt(calculator.values[0]) + parseInt(calculator.values[1]);
+    } else if(op === "-"){
+      result = parseInt(calculator.values[0]) - parseInt(calculator.values[1]);
+    } else if(op === "/"){
+      result = parseInt(calculator.values[0]) / parseInt(calculator.values[1]);
+    } else if(op === "*"){
+      result = parseInt(calculator.values[0]) * parseInt(calculator.values[1]);
     }
 
-    var output = $("#output").html();
-    if(output.length < 10){
-      if(output === "0"){
-        $("#output").html(input);
-      } else {
-        $("#output").html(output + input);
+    $("#output").html(result);
+    calculator.values = [];
+  },
+
+  handleOperation: function(op) {
+    calculator.lastOperation = op;
+    calculator.values.push($("#output").html());
+
+    if(calculator.values.length > 1){
+      calculator.performCalculation(op);
+    }
+
+    $("[data-id='"+op+"']").css("background-color","black");
+    calculator.engagedButtons.push("[data-id='"+op+"']");
+    calculator.clearOnNextInput = true;
+  },
+
+  updateOutput: function(input) {
+    if(calculator.clearOnNextInput){
+
+      $("#output").html(input);
+      calculator.clearOnNextInput = false;
+      calculator.disengageButtons();
+
+    } else {
+      var output = $("#output").html();
+      if(output.length < 10){
+        if(output === "0"){
+          $("#output").html(input);
+        } else {
+          $("#output").html(output + input);
+        }
       }
     }
   },
 
+  disengageButtons: function() {
+    for(var i=0; i<calculator.engagedButtons.length; i++){
+      $(calculator.engagedButtons[i]).css("background-color","gray");
+    }
+    calculator.engagedButtons = [];
+  },
+
   resetOutput: function() {
     $("#output").html("0");
+    for(var i=0; i<calculator.engagedButtons.length; i++){
+      $(calculator.engagedButtons[i]).css("background-color","gray");
+    }
+    calculator.engagedButtons = [];
+    calculator.values = [];
+    calculator.clearOnNextInput = false;
   }
 };
 
